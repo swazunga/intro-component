e0796485bd8ad37f4970ab4e6092206a869da1cc
Michael Kendrick <michael@locumsmart.net>
2022-03-15 17:02:13 -0500

    styling mostly done, working on js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var claimBtn = document.querySelector(".claimBtn");
+entry = [];
+
+function submitForm() {
+  var entryObj = {};
+  console.log("Submit");
+}
+
+claimBtn.addEventListener("click", submitForm);
