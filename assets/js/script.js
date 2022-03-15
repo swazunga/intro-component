aef5bf485f3b8525e0e4fab06a2639c2d757bf01
Michael Kendrick <michael@locumsmart.net>
2022-03-15 18:48:43 -0500

    styling mostly done, working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 var claimBtn = document.querySelector(".claimBtn");
+var form = document.querySelector(".form");
+var first = document.querySelector(".first");
+var last = document.querySelector(".last");
+var email = document.querySelector(".email");
+var password = document.querySelector(".password");
 entry = [];
 
-function submitForm() {
-  var entryObj = {};
-  console.log("Submit");
+function submitForm(event) {
+  event.preventDefault();
+  var entryObj = {
+    first: first.value,
+    last: last.value,
+    email: email.value,
+    password: password.value,
+    id: Math.random(),
+  };
+  console.log(entryObj);
 }
 
 claimBtn.addEventListener("click", submitForm);
