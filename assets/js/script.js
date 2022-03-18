308100e1e0b6e3847a98c4bca3c931400406a5f0
Michael Kendrick <michael@locumsmart.net>
2022-03-17 21:44:19 -0500

    working on form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,40 @@ var first = document.querySelector(".first");
 var last = document.querySelector(".last");
 var email = document.querySelector(".email");
 var password = document.querySelector(".password");
-entry = [];
+var error = document.querySelector(".error");
+entry = JSON.parse(localStorage.getItem("entries")) || [];
 
 function submitForm(event) {
   event.preventDefault();
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("invalid email");
+    email.reportValidity();
+    email.classList.add("inputInvalid");
+    return;
+  }
+
+  if (!first.value) {
+    first.classList.add("inputInvalid");
+    var mySvg = document.createElementNS("http://www.w3.org/2000/svg", "error");
+    mySvg.setAttribute("version", "1.1");
+    first.value = mySvg;
+
+    console.log("wrong");
+    return;
+  }
+  if (!last.value) {
+    last.classList.add("inputInvalid");
+    return;
+  }
+  if (!email.value) {
+    email.classList.add("inputInvalid");
+    return;
+  }
+  if (!password.value) {
+    password.classList.add("inputInvalid");
+    return;
+  }
+
   var entryObj = {
     first: first.value,
     last: last.value,
@@ -15,7 +45,41 @@ function submitForm(event) {
     password: password.value,
     id: Math.random(),
   };
+  entry.push(entryObj);
+  localStorage.setItem("entries", JSON.stringify(entry));
   console.log(entryObj);
 }
+
+first.addEventListener("blur", function () {
+  if (first.value === "") {
+    first.classList.add("inputInvalid");
+  } else {
+    first.classList.remove("inputInvalid");
+  }
+});
+
+last.addEventListener("blur", function () {
+  if (last.value === "") {
+    last.classList.add("inputInvalid");
+  } else {
+    last.classList.remove("inputInvalid");
+  }
+});
+
+email.addEventListener("blur", function () {
+  if (email.value === "") {
+    email.classList.add("inputInvalid");
+  } else {
+    email.classList.remove("inputInvalid");
+  }
+});
+
+password.addEventListener("blur", function () {
+  if (password.value === "") {
+    password.classList.add("inputInvalid");
+  } else {
+    password.classList.remove("inputInvalid");
+  }
+});
 
 claimBtn.addEventListener("click", submitForm);
